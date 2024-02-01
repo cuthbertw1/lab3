38ec6d9eacf70c3b6fd86dcd6806ae9d78b4eb15
Will Cuthbert <cuthbertw1@nku.edu>
2024-02-01 14:35:27 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import os
+def exists(path):
+    if os.path.exists(path):
+        return True
+    else:
+        return False
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def renameFile(targetDirectory, currentExt, newExt):
+    if isAllwoed(newExt):
+        currentExt=newExt
+    else:
+        print("Error: please only enter allowed extentions ")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def isAllwoed(extention):
+    ext=["txt","png","doc","dat"]
+    if extention in ext:
+        return True
+    else:
+        return False
 
+def main():
+    homeDirectory=os.path.expanduser("~")
+    print(homeDirectory)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
