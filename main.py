6fa55ffaa836cd5353c6953f99fabab126a445d3
Will Cuthbert <cuthbertw1@nku.edu>
2024-02-02 13:43:16 -0500

    more functions

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os
-def exists(path):
+
+def exists(path):           #checks is a path exists
+
     if os.path.exists(path):
         return True
     else:
         return False
 
-def renameFile(targetDirectory, currentExt, newExt):
-    if isAllwoed(newExt):
-        currentExt=newExt
+
+def renameFile(fileName, newName):
+
+    if os.path.isfile(fileName):
+        os.rename(fileName, newName)
     else:
-        print("Error: please only enter allowed extentions ")
+        print("Error: file not found ")
 
-
+def createDir(name):
+    if not os.path.isdir(name):
+        os.makedirs(name)
 def isAllwoed(extention):
     ext=["txt","png","doc","dat"]
     if extention in ext:
@@ -19,8 +25,31 @@ def isAllwoed(extention):
     else:
         return False
 
+def getType(fileOrPath):
+    if os.path.isdir(fileOrPath):
+        print("This is a directory")
+    elif os.path.isfile(fileOrPath):
+        print("This is a file")
+    else:
+        print("Not a file or directory")
+
+
+def displayContents(directoryName):
+    if exists(directoryName):
+        contents=os.listdir(directoryName)
+        for content in contents:
+            print(content)
+    else:
+        print("Error: directory not found")
 def main():
-    homeDirectory=os.path.expanduser("~")
-    print(homeDirectory)
+    currentDirectiry=os.getcwd()
+    print(currentDirectiry)
+    fileCheck=input("Select file path or file")
+    getType(fileCheck)
+    nameCheck=input("Enter directory to out contents from: ")
+    displayContents(nameCheck)
+    fileName=input("enter file to rename: ")
+    newName=input("Enter new file name: ")
+    renameFile(fileName.strip(),newName.strip())
 
 main()
